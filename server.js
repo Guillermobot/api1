7dd746fa3b5ffa53ddf03d9eac3dc9da478a591b
Guillermobot <guillermobot10@gmail.com>
2025-04-24 04:04:26 -0600

    cambio /api/pokemon

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 app.use(cors()); // Habilitar CORS para todas las rutas
 app.use(express.json()); // Habilitar el análisis de JSON en las solicitudes
 
-app.use("/api", getroutes); // Usar las rutas definidas en getroutes.js
+app.use("/api/pokemon", getroutes); // Usar las rutas definidas en getroutes.js
 
 app.listen(PORT, () => {
     console.log(`Api Pokemon corriendo en http://0.0.0.0:${PORT}`);
